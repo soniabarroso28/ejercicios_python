e162e92f2c825b6d7594f48ce21d702eef03d4ca
soniabarroso28 <92727665+soniabarroso28@users.noreply.github.com>
2021-10-19 11:30:01 +0200

    Update ejemplo_input_numeros_enteros.py

--- ejemplo_input_numeros_enteros.py
+++ ejemplo_input_numeros_enteros.py
@@ -1,8 +1,5 @@
 numero1 = int(input("Dígame un número: "))
 numero2 = int(input(f"Dígame un número mayor que {numero1}: "))
-if(numero2 < numero1)
-numero2 = int(input(f"Dígame un número mayor que {numero1}: "))
-else if
 print(f"La diferencia entre ellos es {numero2 - numero1}.")
 print(f"La suma entre ellos es {numero2 + numero1}.")
 print(f"La division de ellos es {numero2 / numero1}.")
